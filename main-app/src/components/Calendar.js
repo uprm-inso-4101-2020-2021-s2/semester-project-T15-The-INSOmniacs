8f0942ef111b57b2352d23370aa4ea45cf4be1ac
Alanis Caro <alaniscaro@iMac.local>
2021-04-26 17:35:19 -0400

    Calendar Component Only

--- main-app/src/components/Calendar.js
+++ main-app/src/components/Calendar.js
@@ -1,9 +1,10 @@
 import '../App.css'
-
+import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
 function Calendar() {
   return (
     <div>
         <h1>Calendar Page</h1>
+        <CalendarComponent></CalendarComponent>
     </div>
   );
 }
