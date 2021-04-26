290e8a12049f44040b0f6eeed9d920e337d430aa
Alanis Caro <alaniscaro@iMac.local>
2021-04-26 18:49:00 -0400

    ScheduleCalendar

--- main-app/src/components/Calendar.js
+++ main-app/src/components/Calendar.js
@@ -1,12 +1,17 @@
 import '../App.css'
-import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
+import {Inject,ScheduleComponent,Day,Week,Month,Agenda,WorkWeek} from '@syncfusion/ej2-react-schedule';
+
 function Calendar() {
   return (
     <div>
-        <h1>Calendar Page</h1>
-        <CalendarComponent></CalendarComponent>
+        <h1>Calendar</h1>
+        <ScheduleComponent currentView='Month' selectedDate={new Date(2021,0,11)}>
+          <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+        </ScheduleComponent>
+
     </div>
   );
 }
 
 export default Calendar;
+
